83885854a5008c04dfb6d2ba6988eabb8955c587
Artem <artem96.96@bk.ru>
2019-03-04 18:09:09 +0300

    20

--- 20.php
+++ 20.php
@@ -6,4 +6,40 @@
  * Time: 17:20
  */
 
-function getIntersectionForSortedArray()
+function getIntersectionOfSortedArray($mass1, $mass2)
+{
+    return( array_intersect($mass1, $mass2));
+};
+
+print_r(getIntersectionOfSortedArray([10, 11, 24], [10, 13, 14, 18, 24, 30]));
+
+
+function newgetIntersectionOfSortedArray($mass1, $mass2)
+{
+    if( empty($mass2) || empty($mass2)) { return null; };
+
+
+    $count1 = count($mass1);
+    $count2 = count($mass2);
+    $box = array();
+
+    for ($i = 0; $i < $count1; $i++) {
+        $number = $mass1[$i];
+
+        for ( $ii = 0; $ii < $count2; $ii++ ) {
+
+
+            if ($number === $mass2[$ii]){
+                $box[] = $number;
+                break;
+            };
+        };
+
+    };
+
+    return $box;
+
+};
+
+
+print_r(newgetIntersectionOfSortedArray([], [10, 13, 14, 18, 24, 30]));
